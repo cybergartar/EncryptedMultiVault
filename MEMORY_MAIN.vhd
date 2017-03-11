fd27eabd3703711aba72471d98e4b65ae3f67d3d
Ti Arunrat <kamonnop.a@gmail.com>
2017-03-11 21:46:13 +0700

    [FIX] Edit entity name

--- MEMORY_MAIN.vhd
+++ MEMORY_MAIN.vhd
@@ -29,13 +29,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity MAIN_MEMORY is
+entity MEMORY_MAIN is
     Port ( B3, B2, B1, B0 : in  STD_LOGIC_VECTOR (3 downto 0);
            A3, A2, A1, A0 : out  STD_LOGIC_VECTOR (3 downto 0);
            VST, VFN, ST : in  STD_LOGIC);
-end MAIN_MEMORY;
+end MEMORY_MAIN;
 
-architecture Behavioral of MAIN_MEMORY is
+architecture Behavioral of MEMORY_MAIN is
 	component DFF_2BIT
 		Port ( CLK : in  STD_LOGIC;
            D : in  STD_LOGIC_VECTOR (1 downto 0);
